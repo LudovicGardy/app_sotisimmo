fbcdd6e3e07b7bda8f4e385d618b8c18b35a772a
LudovicGardy <ludovic.gardy@gmail.com>
2023-08-16 22:31:21 +0200

    test 4

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[theme]\n\
-primaryColor=’#020202’\n\
-backgroundColor=’#c4c3c3’\n\
-secondaryBackgroundColor=’#ebd316’\n\
-font = ‘sans serif’\n\
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
