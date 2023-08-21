ba9a61b571d109d991c64646da24fb96572ea22f
LudovicGardy <ludovic.gardy@gmail.com>
2023-08-21 09:32:46 +0200

    added a favicon and a tab title

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ enableCORS = false\n\
 \n\
 [theme]\n\
 base = \"dark\"\n\
+\n\
+[ui]\n\
+favicon = \"https://sotisimmo.s3.eu-north-1.amazonaws.com/Sotis_AI_contrast.ico\"\n\
+headHtml = \"<title>Sotis Immo.</title>\"\n\
 " > ~/.streamlit/config.toml
